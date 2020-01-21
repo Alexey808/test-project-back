76d9de3a6f7d475f60f577fad98dc5dea07dad9f
Alexey808 <laiten808@gmail.com>
2020-01-21 22:19:22 +0400

    fix eslint to generateId

--- src/tools/geterate-id.ts
+++ src/tools/geterate-id.ts
@@ -4,7 +4,8 @@ export function generateId() {
   let str = '';
 
   for (let i = 0; i < 8; i++) {
-    str += sym[parseInt(Math.floor(Math.random() * sym.length - 1) + 1)];
+    // tslint:disable-next-line:radix
+    str += sym[parseInt(String(Math.floor(Math.random() * sym.length - 1) + 1))];
   }
 
   return str;
